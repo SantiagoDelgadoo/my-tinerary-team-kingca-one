aa25990c202b5abec882df0eb8296b81916f1ad0
HernanC95 <cavalieri.h95@gmail.com>
2022-11-06 10:39:42 -0300

    change Names of Variables

--- src/components/CardHotels.jsx
+++ src/components/CardHotels.jsx
@@ -4,31 +4,31 @@ import places from "../data/places";
 import SearchBar from "./FormText";
 
 export default function CardHotels() {
-  let placess = places;
+  let arrayOfPlaces = places;
   let [filterText, SetFilterText] = useState("");
   let [selectValue, setSelectValue] = useState("");
   let inputSearch = (text) => {
     SetFilterText(text.target.value);
   };
 
-  let ArrayFiltered = placess.filter((place) =>
+  let ArrayFiltered = arrayOfPlaces.filter((place) =>
     place.name.toLowerCase().includes(filterText.toLowerCase())
   );
   if ({ ArrayFiltered } === 0) {
-    ArrayFiltered = placess;
+    ArrayFiltered = arrayOfPlaces;
   }
 
-  if(selectValue ==="ASC"){ 
+  if (selectValue === "ASC") {
     ArrayFiltered.sort((a, b) => {
-    if (a.name > b.name) {
-      return 1;
-    }
-    if (a.name < b.name) {
-      return -1;
-    }
-    return 0;
-  });}
-  else if(selectValue ==="DESC"){
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    });
+  } else if (selectValue === "DESC") {
     ArrayFiltered.sort((a, b) => {
       if (a.name > b.name) {
         return -1;
@@ -37,8 +37,9 @@ export default function CardHotels() {
         return 1;
       }
       return 0;
-    });}
-   
+    });
+  }
+
   console.log(ArrayFiltered);
 
   return (
@@ -49,7 +50,10 @@ export default function CardHotels() {
         </div>
         <div>
           <form action="" className="filter-select">
-            <select value={selectValue} onChange={ (event) => setSelectValue( event.target.value)}>
+            <select
+              value={selectValue}
+              onChange={(event) => setSelectValue(event.target.value)}
+            >
               <option value="">Choose an option</option>
               <option value="ASC">Ascending order</option>
               <option value="DESC">Descending order</option>
@@ -61,7 +65,7 @@ export default function CardHotels() {
       {ArrayFiltered.map((place) => {
         return (
           <div className="card">
-            <img className="cardPhoto" src={place.photo[0]} alt="" />
+            <img className="cardPhoto" src={place.photo[0]} alt="photo of Place" />
             <h2 className="titleCard">{place.name}</h2>
           </div>
         );
