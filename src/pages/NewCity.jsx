a014458a890476562046fb437c4227f3a73f8137
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-11 14:51:07 -0300

    arreglos en la task mt-f1-18

--- src/pages/NewCity.jsx
+++ src/pages/NewCity.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useRef } from 'react'
-
+import axios from 'axios'
+let postCity = async function (city){ //creo funcion async xq tiene que esperar, le paso un objeto (city)
+    axios //peticion de axios
+    .post('http://localhost:8000/api/city/',city) //de tipo post, el cual me va a pedir 2 parametros la url y mi objeto q quiero q postee
+    .then((Response)=>console.log(Response)) //despues de hacer eso si se creo exitosamente el .then devuelve una respuesta exitosa
+    .catch((Error)=>console.log(Error)) //si no devuelve una respuesta exitosa, va a devolver error
+}
 export default function NewCity() {
     let NewCityForm = useRef()
     let submit = (e) => {
@@ -9,9 +15,10 @@ export default function NewCity() {
             name:e.target[0].value,
             continent:e.target[1].value,
             photo:e.target[2].value,
-            population:e.target[3].value
+            population:e.target[3].value,
+            userid:"636d39111834aa8ba98269f3"
         }
-        localStorage.setItem("NewCity",JSON.stringify(NewCity))
+        postCity(NewCity)
         NewCityForm.current.reset ()
         console.log(NewCity);
     }
@@ -29,7 +36,7 @@ export default function NewCity() {
             </label>
 
             <label className="labelNewCity"> Photo
-                <input className=".inputSignUp" type="file"  placeholder="Enter photo"/>
+                <input className=".inputSignUp" type="text"  placeholder="Enter photo"/>
             </label>
 
             <label className="labelNewCity"> Population
