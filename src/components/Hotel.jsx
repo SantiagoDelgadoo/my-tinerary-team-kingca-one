fc9a7529f4a1568f0f490f359ecd396bed50f906
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-14 20:58:49 -0300

    task mt-01-21

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 import places from '../data/places';
-export default function Hotel() {
+export default function Hotel(props) {
     let {id} = useParams ()
     let hotelCapture = places.find(place=>place.cityId===id)
     console.log(hotelCapture);
     console.log(id);
+    let {object} = props
   return (
     <div className='cardDetails'>
         <div className='fatherPhoto'>
-        <img src={hotelCapture.photo[0]} className= "ImgDetails" alt="Image Of Cities" />
+        <img src={object.photo} className= "ImgDetails" alt="Image Of Cities" />
         </div>
         <div className='fatherDescription'>
-            <h3 className='titleCities'>{hotelCapture.name}</h3>
-            <p className='descriptionCities'>{hotelCapture.capacity}</p>
-            <p className='descriptionCities'>{hotelCapture.description}</p>
+            <h3 className='titleCities'>{object.name}</h3>
+            <p className='descriptionCities'>{object.capacity}</p>
+            <p className='descriptionCities'>{object.description}</p>
         </div>
     </div>
   )
