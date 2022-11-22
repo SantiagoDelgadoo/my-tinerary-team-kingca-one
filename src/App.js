4c6853e6f2e587cb109b2b17be2140d36b28621a
SantiagoDelgadoo <santiagodelgado.2003@gmail.com>
2022-11-22 09:25:46 -0300

    task lista

--- src/App.js
+++ src/App.js
@@ -18,9 +18,8 @@ import NewCity from './pages/NewCity';
 import DetailsHotel from './pages/DetailsHotel';
 import DetailsCities from './pages/DetailsCities';
 import NewHotel from './pages/NewHotel';
-
+import MyCities from './pages/MyCities';
 import MyShows from './pages/MyShows';
-
 import MyIneraries from './pages/MyIneraries';
 
 function App() {
@@ -40,9 +39,8 @@ function App() {
     <Route path="/details/:id" element={<DetailsCities/>}></Route>
     <Route path="/detailsHotel/:id" element={<DetailsHotel/>}></Route>
     <Route path="/newhotel" element={<NewHotel/>}></Route>
-
     <Route path="/myshows" element={<MyShows/>}></Route>
-
+    <Route path="/mycities" element={<MyCities/>}></Route>
     <Route path="/myitineraries" element={<MyIneraries/>}></Route>
 
     </Routes>
--- src/redux/actions/citiesAction.js
+++ src/redux/actions/citiesAction.js
@@ -29,5 +29,44 @@ const allCities = createAsyncThunk ('allCities',async ()=>{
     }
 
 })
-const citiesAction = {getCities,allCities}
+const getMyCities = createAsyncThunk ('getMyCities',async ()=>{
+    const user = "636d39111834aa8ba98269f2"
+    try {
+        const res = await axios.get (`${base_url}/city/?userId=${user}`)
+        return {
+            listCities: res.data.cities
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+const deleteMyCities = createAsyncThunk ('deleteMyCities',async (data)=>{
+    const id = data.id
+    try {
+        const res = await axios.delete(`${base_url}/city/${id}`)
+        return {
+            mycitiesdeleted: res.data.id
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+const editCity = createAsyncThunk ('editCity',async (data)=>{
+    const {id, info} = data;
+    try {
+        const res = await axios.put(`${base_url}/city/${id}`,info, {new: true}
+        );
+        console.log(res.data);
+        return {
+            cityedit: res.data.id
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+})
+
+
+const citiesAction = {getCities,allCities,getMyCities,deleteMyCities, editCity }
 export default citiesAction
